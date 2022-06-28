edf1185c7088db0d625435cd007a06b6899a21d5
Cindy <caps20313@hotmail.com>
2022-06-29 01:54:09 +0800

    update

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
+import Navigation from './Navigation';
 import mealsImage from "../../assets/meals.jpg";
 
 const Header = (props) => {
@@ -8,6 +9,8 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
+        
+        <Navigation />
         <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
